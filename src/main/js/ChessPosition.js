24c9745b1f2632cbef3a3ee1179f621a378fa2e2
Piotr Ostrowski <piotr.ostrowski.se@gmail.com>
2021-12-04 13:59:32 +0100

    added moving pieces by drag&drop

--- src/main/js/ChessPosition.js
+++ src/main/js/ChessPosition.js
@@ -1,13 +1,24 @@
 import parseFen from './parseFen.js';
 
 class ChessPosition {
-    constructor(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
-        this.fen = fen;
-        this.pieces = parseFen(fen);
+    constructor(pieces) {
+        this.pieces = pieces;
     }
 
     getPiece(coordinate) {
         return this.pieces.get(coordinate);
+    }
+
+    copy() {
+        return new ChessPosition(new Map(this.pieces));
+    }
+
+    static fromFen(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
+        return new ChessPosition(parseFen(fen));
+    }
+
+    static default() {
+        return this.fromFen();
     }
 }
 
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -13,49 +13,66 @@ class Piece extends React.Component {
     }
 
     onMouseDown(event) {
-        document.addEventListener('mousemove', this.onMouseMove);
-        document.addEventListener('mouseup', this.onMouseUp);
+        if(event.button === 0) { // left click
+            document.addEventListener('mousemove', this.onMouseMove);
+            document.addEventListener('mouseup', this.onMouseUp);
 
-        this.startingX = event.nativeEvent.pageX - event.nativeEvent.offsetX + this.divElement.clientWidth / 2;
-        this.startingY = event.nativeEvent.pageY - event.nativeEvent.offsetY + this.divElement.clientHeight / 2;
+            this.startingX = event.nativeEvent.pageX - event.nativeEvent.offsetX + this.divElement.clientWidth / 2;
+            this.startingY = event.nativeEvent.pageY - event.nativeEvent.offsetY + this.divElement.clientHeight / 2;
 
-        this.setState({
-            x: event.pageX - this.startingX,
-            y: event.pageY - this.startingY,
-            dragging: true
-        });
+            this.offsetX = 100 * (this.props.position.charCodeAt(0) - 'a'.charCodeAt(0));
+            this.offsetY = 100 * (8 - Number(this.props.position.charAt(1)));
+
+            const position = this.calculatePosition(event);
+
+            this.setState({
+                x: position.x,
+                y: position.y,
+                dragging: true
+            });
+        }
+    }
+
+    calculatePosition(event) {
+        let factor = 100 / this.divElement.clientWidth;
+        return {
+            x: (event.pageX - this.startingX) * factor + this.offsetX,
+            y: (event.pageY - this.startingY) * factor + this.offsetY,
+        };
     }
 
     onMouseUp = () => {
-        document.removeEventListener('mousemove', this.onMouseMove);
-        document.removeEventListener('mouseup', this.onMouseUp);
+        if(event.button === 0) { // left click
+            document.removeEventListener('mousemove', this.onMouseMove);
+            document.removeEventListener('mouseup', this.onMouseUp);
 
-        this.setState({
-            x: 0,
-            y: 0,
-            dragging: false
-        });
+            this.setState({
+                x: 0,
+                y: 0,
+                dragging: false
+            });
+            this.props.onMouseUp();
+        }
     }
 
     onMouseMove = (event) => {
-        this.setState({
-            x: event.pageX - this.startingX,
-            y: event.pageY - this.startingY
-        });
+        this.setState(this.calculatePosition(event));
     }
 
     render() {
-        const style = {
-            zIndex: this.state.dragging ? 1000 : 0,
-            pointerEvents: this.state.dragging ? 'none' : 'inherit',
-            transform: 'translate(' + this.state.x + 'px, ' + this.state.y + 'px)'
+        const style = !this.state.dragging ? {} : {
+            zIndex: 1000,
+            pointerEvents: 'none',
+            transform: 'translate(' + this.state.x + '%, ' + this.state.y + '%)'
         }
 
         return (
             <div
                 style={style}
-                className={'draggable div-image piece ' + this.props.piece}
+                className={'draggable div-image piece ' + this.props.piece + ' ' + this.props.position}
                 onMouseDown={this.onMouseDown.bind(this)}
+                onMouseEnter={() => this.props.onMouseEnter(this.props.position)}
+                onMouseLeave={() => this.props.onMouseLeave(this.props.position)}
                 ref={ divElement => { this.divElement = divElement } }
             />
         );
@@ -69,48 +86,78 @@ class Square extends React.Component {
     }
 
     render() {
-        const coordinate = String.fromCharCode('a'.charCodeAt(0) + this.props.x) + (8 - this.props.y);
-        const imageName = coordinate === 'd4' ? 'd6' : coordinate; // missing d4 image
+        const imageName = this.props.position === 'd4' ? 'd6' : this.props.position; // missing d4 image
         const imagePath = "./Chess_Artwork/Chess_Board/Stone_Grey/" + imageName + ".png";
 
         const style = {
             backgroundImage: "url('" + imagePath + "')"
         };
 
-        const chessPiece = this.props.position.getPiece(coordinate);
-
         return (
             <div
-                onMouseEnter={() => this.setState({highlight: true})}
-                onMouseLeave={() => this.setState({highlight: false})}
-                key={coordinate}
+                onMouseEnter={() => this.props.onMouseEnter(this.props.position)}
+                onMouseLeave={() => this.props.onMouseLeave(this.props.position)}
+                key={this.props.position}
                 style={style}
-                className={"unselectable div-image square" + (this.state.highlight ? " highlight" : "")}
-            >
-                {chessPiece !== null && <Piece piece={chessPiece} />}
-            </div>
+                className={"unselectable div-image square" + (this.props.highlight === this.props.position ? " highlight" : "")}
+            />
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chessPosition: ChessPosition.default(),
+            highlight: null
+        }
+    }
+
+    move(from) {
+        if(this.state.highlight !== null && from !== this.state.highlight) {
+            const newPosition = this.state.chessPosition.copy();
+            newPosition.pieces.set(this.state.highlight, newPosition.getPiece(from));
+            newPosition.pieces.set(from, null);
+
+            this.setState({chessPosition: newPosition});
+        }
+    }
+
     render() {
-        const chessPosition = new ChessPosition();
-
         const squares = Array.from({length: 64})
-            .map((_, i) => <Square key={i} x={i % 8} y={Math.floor(i / 8)} position={chessPosition}/>)
+            .map((_, i) => String.fromCharCode('a'.charCodeAt(0) + (i % 8)) + (8 - Math.floor(i / 8)))
+            .map((position, i) => <Square
+                key={i}
+                position={position}
+                highlight={this.state.highlight}
+                onMouseEnter={pos => this.setState({highlight: pos})}
+                onMouseLeave={() => this.setState({highlight: null})}
+            />)
 
-
+        const pieces = Array.from(this.state.chessPosition.pieces.entries())
+            .filter(([, value]) => value !== null)
+            .map(([position, piece]) =>
+                <Piece
+                    key={position}
+                    position={position}
+                    piece={piece}
+                    onMouseEnter={pos => this.setState({highlight: pos})}
+                    onMouseLeave={() => this.setState({highlight: null})}
+                    onMouseUp={() => this.move(position)}
+                />);
 
         return (
-            <div className="board">
+            <div className="board" onContextMenu={event => event.preventDefault()}>
                 <div className="square-container">
                     {squares}
                 </div>
+                {pieces}
             </div>
         )
     }
 }
+
 
 ReactDOM.render(
     <Board/>,
